b56c9e55f9e0620d10170c0a3d9f929358a0e307
ThePigStupo <olannbrink7@gmail.com>
2020-08-28 11:00:47 +0200

    Setup

---
+++ src/bot.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const { Client } = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in.`);
+});
+
+client.on('message', (message) => {
+
+})
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
